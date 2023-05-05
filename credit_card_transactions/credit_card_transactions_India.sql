a1dd85dd82d7260fc2fd8a5fb08ab585e60a2810
Neelam Kushwaha <94687063+neelam1783@users.noreply.github.com>
2023-05-05 18:40:07 +0200

    Update credit_card_transactions_India.sql

--- credit_card_transactions/credit_card_transactions_India.sql
+++ credit_card_transactions/credit_card_transactions_India.sql
@@ -1,3 +1,32 @@
+/* SQL porfolio project.
+
+download credit card transactions dataset from below link :
+https://www.kaggle.com/datasets/thedevastator/analyzing-credit-card-spending-habits-in-india
+import the dataset in mysql with table name : credit_card
+
+while importing make sure to change the data types of columns.
+
+write the queries to explore the dataset and put your findings 
+
+solve below questions
+
+1- write a query to print top 5 cities with highest spends and their percentage contribution of total credit card spends 
+
+2- write a query to print highest spend month and amount spent in that month for each card type
+3- write a query to print the transaction details(all columns from the table) for each card type when
+it reaches a cumulative of 1000000 total spends(We should have 4 rows in the o/p one for each card type)
+
+4- write a query to find city which had lowest percentage spend for gold card type
+
+5- write a query to print 3 columns:  city, highest_expense_type , lowest_expense_type (example format : Delhi , bills, Fuel)
+
+6- write a query to find percentage contribution of spends by females for each expense type
+
+7- which card and expense type combination saw highest month over month growth in Jan-2014
+9- during weekends which city has highest total spend to total no of transcations ratio  
+
+*/
+
 create database sql_portfolio;
 use sql_portfolio;
 
