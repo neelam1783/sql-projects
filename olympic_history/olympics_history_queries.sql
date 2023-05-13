cff045c21e8e6c4391b613a76bbbf47602292760
Neelam Kushwaha <94687063+neelam1783@users.noreply.github.com>
2023-05-13 14:13:06 +0200

    Update olympics_history_queries.sql

--- olympic_history/olympics_history_queries.sql
+++ olympic_history/olympics_history_queries.sql
@@ -1,4 +1,34 @@
-use ankitbansal_database;
+Datset is downloaded from Kaggel.
+Create the table structure in MYsql.
+I have solved all the queries in MYsql
+
+/* List of all these 19  queries mentioned below:
+
+How many olympics games have been held?
+List down all Olympics games held so far.
+Mention the total no of nations who participated in each olympics game?
+Which year saw the highest and lowest no of countries participating in olympics?
+Which nation has participated in all of the olympic games?
+Identify the sport which was played in all summer olympics.
+Which Sports were just played only once in the olympics?
+Fetch the total no of sports played in each olympic games.
+Fetch details of the oldest athletes to win a gold medal.
+Fetch the top 5 athletes who have won the most gold medals.
+Fetch the top 5 athletes who have won the most medals (gold/silver/bronze).
+Fetch the top 5 most successful countries in olympics. Success is defined by no of medals won.
+List down total gold, silver and broze medals won by each country.
+List down total gold, silver and broze medals won by each country corresponding to each olympic games.
+Identify which country won the most gold, most silver and most bronze medals in each olympic games.
+Identify which country won the most gold, most silver, most bronze medals and the most medals in each olympic games.
+Which countries have never won gold medal but have won silver/bronze medals?
+In which Sport/event, India has won highest medals.
+Break down all olympic games where india won medal for Hockey and how many medals in each olympic games.
+
+
+
+*/
+
+
 select count(*) from olympics_history_noc_regions;
 /* 1.How many olympics games have been held? */
 desc olympics_history;
